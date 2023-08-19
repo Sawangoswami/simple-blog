f4b9789dd29db0a1f8611e79c34a252c8955686d
sawan kumar <91118790+sawangoswami@users.noreply.github.com>
2023-08-19 23:17:16 +0530

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -19,11 +19,11 @@
         include 'config.php';
         $limit = 3;
         $offset = 0;
-       $sql = "SELECT * FROM post 
+        $sql = "SELECT * FROM post 
        LEFT JOIN category ON post.category = category.category_id
        LEFT JOIN user ON post.author = user.user_id
        ORDER BY Post.post_id DESC LIMIT  {$offset}, {$limit}";
-       $result = mysqli_query($conn, $sql);
+       $result = mysqli_query($conn, $sql) or die("Query Failed");
        
        if(mysqli_num_rows($result) > 0){
        while($row = mysqli_fetch_assoc($result))  {?>
