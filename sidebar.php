456d4c7b18d32bc3c14bb865167a259d4a515eea
sawan kumar <91118790+sawangoswami@users.noreply.github.com>
2023-08-19 16:02:22 +0530

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -18,7 +18,7 @@
         <?php 
         error_reporting(E_ALL);
         ini_set('display_errors', '1');
-       //include 'config.php';
+       include'config.php';
         $limit = 3;
         $offset = 0;
        $sql = "SELECT * FROM post 
